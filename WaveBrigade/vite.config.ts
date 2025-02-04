c2fea1d1dc66c19b05c907f1096a8260a14c3a1e
Haley Marquez <89376070+Marquez-Haley@users.noreply.github.com>
2025-02-04 13:59:00 -0800

    vite config

--- WaveBrigade/vite.config.ts
+++ WaveBrigade/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "Marquez-Haley/WaveBrigade",
   plugins: [react(), tailwindcss()],
   
 })
